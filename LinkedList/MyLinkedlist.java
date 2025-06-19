3f30db0d0e602cfcea3b42f58bbcf386042ccd92
Ganesh Gundekarla <157367434+gnevercodes@users.noreply.github.com>
2025-06-18 21:41:43 -0500

    Add files via upload

---
+++ LinkedList/MyLinkedlist.java
@@ -0,0 +1,33 @@
+//
+
+public class MyLinkedlist {
+
+    Node head;
+
+    public void add(int data) {
+
+        Node node = new Node(); // creating 1st , 2nd , 3rd nodes and soo on . 
+        node.data = data; // addign the data frm RUNNEr --> MyLinkedList --> Node ; by obj
+        if (head == null) { // checking if the node is the first one or not . 
+            head = node;
+        } else {
+            Node n = head; // adding a other node and making it head / assume it as second node.
+            while (n.next != null) {
+
+                n = n.next;
+            }
+            n.next = node;
+        }
+
+    }
+
+    public void show() {
+        Node node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+}
---
+++ LinkedList/Runner.java
@@ -0,0 +1,13 @@
+
+public class Runner {
+
+    public static void main(String[] args) {
+        MyLinkedlist list = new MyLinkedlist();
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        list.add(3);
+
+        list.show();
+    }
+}
