155e8a2956541950a7f42e3aac1fd1bc6d9810e4
Ganesh Gundekarla <157367434+gnevercodes@users.noreply.github.com>
2025-07-07 21:57:50 -0500

    Add files via upload

---
+++ StringandStringBuilder/strOutput.java
@@ -0,0 +1,10 @@
+
+public class strOutput {
+
+    public static void main(String[] args) {
+
+        int one = 2;
+        System.out.println(one);
+        System.out.println(new int[]{2, 3, 4});
+    }
+}
