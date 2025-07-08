73fb4fcede40507dbdd8a602de030c73f7b9d538
Ganesh Gundekarla <157367434+gnevercodes@users.noreply.github.com>
2025-07-07 21:57:40 -0500

    Add files via upload

---
+++ StringandStringBuilder/stringBuilders.java
@@ -0,0 +1,28 @@
+
+public class stringBuilders {
+
+    public static void main(String[] args) {
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch = ((char) ('a' + i));
+            builder.append(ch); // n + (n+1) + (n+2) ==== nsquared = tc. 
+            /* which is bad time complexity 
+             * if there is a datatype we can directly add and make changes in the same object.
+             * series = abcd + e = abcde ( no new object )
+             * THIS CAN ONLY TAKE N ITERATIONS. 
+             * Yes 
+             ! this datatype is called as "STRINGBUILDER"   
+             */
+
+        }
+        builder.append("s");
+        int something = builder.charAt(22);
+        System.out.println(something);
+        System.out.println(builder.toString());
+        System.out.println(builder.reverse());
+        /* 
+        ! this takes N iterations only . 
+         */
+    }
+}
