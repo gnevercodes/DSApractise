68788045dc10948bf045525aeb431c7bde59e04d
Ganesh Gundekarla <157367434+gnevercodes@users.noreply.github.com>
2025-07-07 21:57:28 -0500

    Add files via upload

---
+++ StringandStringBuilder/prettyPrinting.java
@@ -0,0 +1,15 @@
+
+public class prettyPrinting {
+
+    public static void main(String[] args) {
+        float a = 534.44444f;
+        System.out.printf("formatted number is %.2f", a);
+        System.out.printf("the value of PI is %.3f", Math.PI);
+        System.out.println();
+        System.out.printf("hello my name is %s and I am %s ", "Gnaehs", "newbie ");
+        /*
+         *  place holders
+         * %c, %d, %e %f , %n , 
+         */
+    }
+}
