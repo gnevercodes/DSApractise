dd17d5fc3b98e7ac375b2261a53a5219e5666fb6
Ganesh Gundekarla <157367434+gnevercodes@users.noreply.github.com>
2025-07-10 09:29:20 -0500

    Add files via upload

---
+++ LinkedList/LLbyKunal/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        doublelink one = new doublelink();
+        one.insertFirst(2);
+        one.insertFirst(4);
+        one.insertFirst(5);
+        one.insertFirst(7);
+        one.insertFirst(8);
+
+        one.displayForwardBackward();
+    }
+}
