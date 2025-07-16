f4bca7a8fbe376f12229cd9bb735ef33d12515f7
ganesh <gnevercodes@gmail.com>
2025-07-16 09:54:10 -0500

    constructors

--- timeComplexity/recapBasics/basics.java
+++ timeComplexity/recapBasics/basics.java
@@ -2,4 +2,18 @@ package timeComplexity.recapBasics;
 
 public class basics {
 
+    String name;
+
+    public static void main(String[] args) {
+        // stacks and heaps 
+        basics one = new basics("ganesh");
+
+    }
+    // constructor overloading . 
+    // we need to have a empty one if we create a custom constructor. 
+
+    basics(String name) {
+        this.name = name; // this naming convention is used when referring , assigning. 
+        System.out.println(name);
+    }
 }
