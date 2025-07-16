89310b72a3a93040d6fb1d87c97bdc5910f5ab1b
ganesh <gnevercodes@gmail.com>
2025-07-16 12:22:58 -0500

    static

--- timeComplexity/recapBasics/statics.java
+++ timeComplexity/recapBasics/statics.java
@@ -2,4 +2,32 @@ package timeComplexity.recapBasics;
 
 public class statics {
 
+    public static void main(String[] args) {
+        Human one = new Human(22, "ganesh", 45000, false);
+        Human two = new Human(22, "ganesh", 45000, false);
+        System.out.println(one.population);
+        System.out.println(two.population);
+
+    }
+
 }
+
+class Human {
+
+    int age;
+    String name;
+    int salary;
+    boolean marrierd;
+    static long population; // this can be accessed before the object is being created also . 
+
+    public Human(int age, String name, int salary, boolean marrierd) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+        this.marrierd = marrierd;
+        Human.population += 1; // static variables are apart from instance variables. 
+        // * follow convention
+    }
+
+}
+// ? properties that are not related to the object
