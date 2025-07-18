184b643da39b6ac9ecc7dd058df3d99ef8265ba0
ganesh <gnevercodes@gmail.com>
2025-07-18 16:37:15 -0500

    1

---
+++ timeComplexity/class4/abstractInterfaces.java
@@ -0,0 +1,18 @@
+package timeComplexity.class4;
+
+abstract public class abstractInterfaces {
+
+    // abstract methods gives us a template. 
+    // child class must override this thing.
+    // ! must override it. 
+    // class must be declared "abstract" if it has a method has "abstract" methods. 
+    // USE Case : if those methods defly needs to be implemented. 
+    abstract void career(String name);
+
+    abstract void partner(String name, int age);
+
+    static void something() {
+        System.out.println("blah");
+    }
+
+}
