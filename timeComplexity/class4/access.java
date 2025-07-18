8b45439d1742a1074e790d9add2fea02aa8eaa3e
ganesh <gnevercodes@gmail.com>
2025-07-18 12:33:34 -0500

    access modifiers.

--- timeComplexity/class4/access.java
+++ timeComplexity/class4/access.java
@@ -2,7 +2,23 @@ package timeComplexity.class4;
 
 public class access {
 
-    int num;
-    String name;
+    private int num;
+    private String name;
+
+    public int getNum() { // getters and setters . should be public. 
+        return num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
---
+++ timeComplexity/class4/readme.md
@@ -0,0 +1,11 @@
+Public means you can access it anywhere
+private means only it's file can access it.
+default means you will not be able to access it outside the package.
+protected
+
+    | classs | package | subclass(same package ) | subclass( with different package ) | world(diff package and not a subclass)
+
+public + + + + +
+protected +. + + + +
+default + +. + - -
+private. + - - - -
