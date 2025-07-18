c04dc811703ce93ce34574b746f7885635844c77
ganesh <gnevercodes@gmail.com>
2025-07-18 16:37:26 -0500

    2

---
+++ timeComplexity/class4/main.java
@@ -0,0 +1,12 @@
+package timeComplexity.class4;
+
+public class main {
+
+    public static void main(String[] args) {
+        son one = new son();
+        one.career("blah");
+        one.partner("jolie", 22);
+
+    }
+
+}
