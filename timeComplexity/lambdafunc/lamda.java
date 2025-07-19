db9e4ae0105235b58987d7b192027d4144508caa
ganesh <gnevercodes@gmail.com>
2025-07-18 23:32:47 -0500

    lambda functions basics

---
+++ timeComplexity/lambdafunc/lamda.java
@@ -0,0 +1,23 @@
+package timeComplexity.lambdafunc;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class lamda {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arr.add(i + 1);
+        }
+        // arr.forEach((item) -> System.out.println(item * 2));
+
+        Consumer<Integer> fun = (item) -> System.out.println(item * 2);
+
+        arr.forEach(fun);
+    }
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
