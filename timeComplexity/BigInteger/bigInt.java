308a97f20d8a14adfff911d752c15f2887883c30
ganesh <gnevercodes@gmail.com>
2025-07-22 18:17:47 -0500

    added bigInteger learner file

---
+++ timeComplexity/BigInteger/bigInt.java
@@ -0,0 +1,16 @@
+package timeComplexity.BigInteger;
+
+import java.math.BigInteger;
+
+public class bigInt {
+
+    public static void main(String[] args) {
+        int a = 20;
+
+        BigInteger b = BigInteger.valueOf(33);
+        BigInteger c = BigInteger.valueOf(333345353);
+
+        BigInteger s = c.add(b);
+
+    }
+}
