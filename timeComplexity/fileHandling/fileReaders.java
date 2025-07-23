dfceb0911bb015c138d048b3befaf5abcb91c3e2
ganesh <gnevercodes@gmail.com>
2025-07-23 11:51:03 -0500

    as

---
+++ timeComplexity/fileHandling/fileReaders.java
@@ -0,0 +1,12 @@
+package timeComplexity.fileHandling;
+
+import java.io.*;
+
+public class fileReaders {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
