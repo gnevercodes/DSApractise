50d9ccc5288428d8a2d290f6b0f6ff1838d815ea
ganesh <gnevercodes@gmail.com>
2025-07-24 01:16:09 -0500

    binarySearch recursion vmethod added.

--- recursion/questionsRecursion.java
+++ recursion/questionsRecursion.java
@@ -2,8 +2,14 @@
 public class questionsRecursion {
 
     public static void main(String[] args) {
-        int ans = fibonacciNumber(4);
-        System.out.println(ans);
+        // int ans = fibonacciNumber(4);
+        // System.out.println(ans);
+        int[] arrays = {1, 2, 3, 4, 5, 6, 8, 10};
+        int target = 6;
+        int start = 0;
+        int end = arrays.length - 1;
+        int ans2 = binarySearchRecursion(arrays, target, start, end);
+        System.out.println(ans2);
 
     }
 
@@ -16,5 +22,21 @@ public class questionsRecursion {
         return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
 
     }
+
     // # question 2
+    static int binarySearchRecursion(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1; // # edge case 
+        }
+        int middle = start + (end - start) / 2;
+
+        if (arr[middle] == target) {
+            return middle;
+        } else if (arr[middle] > target) {
+            return binarySearchRecursion(arr, target, start, middle - 1);
+        } else {
+            return binarySearchRecursion(arr, target, middle + 1, end);
+        }
+
+    }
 }
