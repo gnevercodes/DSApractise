16286f02fd3a3b321a84859fe9231e1b10b3953c
ganesh <gnevercodes@gmail.com>
2025-12-04 11:10:08 -0600

    it starts

---
+++ recapDelete/recapdeletsoon.java
@@ -0,0 +1,15 @@
+package recapDelete;
+
+import java.util.*;
+
+public class recapdeletsoon {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> one = new ArrayList<Integer>();
+        /* arraylist takes only objects dumbo */
+        one.add(4);
+        Boolean isIn = one.contains(4);
+        System.out.println(isIn);// Autoboxing: int 4 is automatically converted to Integer 
+
+    }
+}
