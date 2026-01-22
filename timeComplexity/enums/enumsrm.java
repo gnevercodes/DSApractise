be5cab0329161de4df6380dcbfec71981a5817e3
ganesh <gnevercodes@gmail.com>
2026-01-22 01:35:31 -0600

    add enumsrm enum with HIGH, LOW, and MEDIUM values

---
+++ timeComplexity/enums/enumsrm.java
@@ -0,0 +1,15 @@
+package timeComplexity.enums;
+
+public enum enumsrm { // 
+    HIGH,
+    LOW,
+    MEDIUM // like a class file , this is a enum file. 
+}
+
+class now {
+
+    public static void main(String[] args) {
+        enumsrm one = new enumsrm();
+
+    }
+}
