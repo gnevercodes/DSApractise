dbe140012f5afed0a20c10cddf83b5587c9aeb2c
ganesh <gnevercodes@gmail.com>
2026-01-30 16:45:16 -0600

    trees implementation BST Remaining

--- Trees/customTrees.java
+++ Trees/customTrees.java
@@ -14,9 +14,10 @@ public class customTrees { // ! Binary Tree.
         System.out.println("enter the root node: ");
         int value = scanner.nextInt();
         root = new Node(value);
-        populate(scanner, root);
+        populate(scanner, root); // goes from here to popualate(scanner,node). 
     }
 
+    // ! very logical code. (TREES + CALLStack)
     public void populate(Scanner scanner, Node node) {
         System.out.println("do you want to enter left of " + node.value);
         boolean left = scanner.nextBoolean();
@@ -27,7 +28,29 @@ public class customTrees { // ! Binary Tree.
             populate(scanner, node.left);// calling the recursion again. 
 
         }
+        System.out.println("do you want to enter right of " + node.value);
+        boolean right = scanner.nextBoolean();
+        if (right) {
+            System.out.println("enter the value to the right" + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner, node.right);// calling the recursion again. 
 
+        }
+
+    }
+
+    public void display() {
+        display(root, "");
+    }
+
+    private void display(Node node, String indent) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
     }
 
     private static class Node {
@@ -39,6 +62,12 @@ public class customTrees { // ! Binary Tree.
         public Node(int value) {
             this.value = value;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        customTrees tres = new customTrees();
+        tres.populate(scanner);
     }
 
 }
